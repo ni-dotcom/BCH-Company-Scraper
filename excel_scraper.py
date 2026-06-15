c1f28003c17f58583c08cc82cb747d1b7fc75b82
Nithya <126185039+ni-dotcom@users.noreply.github.com>
2026-06-15 16:15:10 -0400

    excel scraper
    Added functionality to scrape additional companies with their descriptions from an Excel file.

---
+++ excel_scraper.py
@@ -0,0 +1,14 @@
+# scrape additional companies with descriptions in excel
+# use with main_scraper.py
+
+excel = pd.read_excel("CompaniesExport.xlsx")
+
+excel["Scraped Text"] = excel["Company type(s)"].astype(str) + "\n" + excel["Brief description"].astype(str) + "\n" + excel["Description"].astype(str) + "\n" + excel["Primary therapeutic area(s)"].astype(str) + "\n" + excel["Secondary therapeutic area(s)"].astype(str) + "\n" + excel["Partnering objectives"].astype(str) + "\n" + excel["Company objectives"].astype(str)
+
+excel["Categories Predicted"] = excel["Scraped Text"].apply(lambda text: match_keywords(text, flat_keyword_map, flat_therapy_map))
+
+out = "Excel_scrape.xlsx"
+with pd.ExcelWriter(out, engine='openpyxl', mode='w') as writer:
+  excel.to_excel(writer, sheet_name="All Data", index=False)
+
+files.download(out)
