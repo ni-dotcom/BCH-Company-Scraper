d6f2904b95f69ba332fb541d920411fa8d302e79
Nithya <126185039+ni-dotcom@users.noreply.github.com>
2026-07-02 13:34:30 -0400

    Modify website search and output handling
    Updated the input Excel file and enhanced URL search functionality with error handling. Added output to separate sheets for found and not found websites.

--- add_websites.py
+++ add_websites.py
@@ -5,14 +5,26 @@ import requests
 
 ddgs = DDGS(api_url="http://localhost:4479", spawn_api=True)
 
-df = pd.read_excel("Companies_NeedsWebsites.xlsx")
+df = pd.read_excel("Companies_NeedsWebsites_test.xlsx")
 
 def find_urls(name):
   if isinstance(name, str):
-    search = DDGS().text(name, max_results=1)
-    url = search.get("href")
-    return url
+    try:
+      search = ddgs.text(f"{name} -site:wikipedia.org", max_results=1)
+      print(f"{name} gives {search[0]}")
+      url = search[0].get("href")
+      return url
+    except Exception as e:
+      print(e)
+      return None
   else:
-    return none
+    print(f"{name} isn't a string")
+    return None
 
 df["Website"] = df["Name"].apply(find_urls)
+not_found = df[df["Website"].isna()].copy()
+
+output = "Websites_given.xlsx"
+with pd.ExcelWriter(output, engine='openpyxl', mode='w') as writer:
+  df.to_excel(writer, sheet_name="All", index=False)
+  not_found.to_excel(writer, sheet_name="Not found", index=False)
